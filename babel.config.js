0699a8eac9199c5fdafe06e11b130cd77a9d5fa0
Jânderson Machado <jandersonmachado090@gmail.com>
2024-11-20 20:19:29 -0300

    remove text

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',  // Para compilar o JavaScript moderno
-    '@babel/preset-react' // Para compilar JSX
+    '@babel/preset-env',  
+    '@babel/preset-react' 
   ]
 };
